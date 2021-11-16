ffa3126ec68692652d855cca5f96633500cfbd96
Dario Reis <guttyerresjunior1995@gmail.com>
2021-11-16 18:13:56 -0300

    adiciona conteudo e atualiza o README.md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aula_javascript
 Introdução ao JavaScript
+
+Temas estudados
+
+Como inserir um arquivo js ao html
+comando alert
+
+o que e variavel e como ultiliza-la 
+
+como usar o console.log
+
---
+++ js/main.js
@@ -0,0 +1,19 @@
+//alert("Minha Primeira página js");
+
+/*var nome = "Dario Reis";
+alert("seja bem vindo " + nome + " ao nosso site");*/
+
+/*var nome = "Anderson Silva";
+var idade = 42;
+var idade2 = 11;
+var frase = "O maior clube do Brasil e o Flamengo";
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Flamengo", "Corinthians"));*/
+
+var lista = ["Atletico Mg", "Flamengo", "Corinthians", "Grêmio" ]
+//lista.push("Vasco")
+lista.pop("Grêmio")
+lista.push("Vasco")
+console.log(lista)
